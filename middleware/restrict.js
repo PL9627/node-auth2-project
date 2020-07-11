68df235667fe776996947b08cde21ae77ab8dcef
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-10 17:48:55 -0700

    added dependencies and created config, index, restrict, and knexfile files

---
+++ middleware/restrict.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+
+const roles = ["normal", "admin"];
+
+function restrict() {
+  return async (req, res, next) => {
+    const authErr = {
+      message: "Invalid Credentials",
+    };
+
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        return res.status(401).json(authErr);
+      }
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json(authErr);
+        }
+
+        if (role && roles.indexOf(decoded.userRole) < roles.indexOf(role)) {
+          return res.status(401).json(authErr);
+        }
+      });
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+module.exports = restrict;
